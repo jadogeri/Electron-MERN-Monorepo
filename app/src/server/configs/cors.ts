84c34578b2df6e5dc7ef0894280d7011041e43df
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-11-03 22:07:43 -0600

    Added cors.ts

---
+++ app/src/server/configs/cors.ts
@@ -0,0 +1,14 @@
+/**
+ * @author      Joseph Adogeri
+ * @since       27-AUG-2024
+ * @version     2.0
+ * @description configuration setting for cors
+ *  
+ */
+export const corsOptions = {
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}
+
